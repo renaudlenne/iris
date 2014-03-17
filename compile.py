becf9b613e3a56608cab42c43fda7921bb21be85
gjyeates <gjyeates@dc-cloud104-n3.cloud.cs.uwaterloo.ca>
2014-03-17 17:32:24 -0400

    Add submodule update to compile.py

--- compile.py
+++ compile.py
@@ -2,10 +2,16 @@ import subprocess, shlex
 
 working_dir = r"static"
 
+#update submodules to latest version ie static
+submod_update = subprocess.Popen(shlex.split("git submodule foreach git pull origin master"), shell=True)
+submod_update.wait() #wait to complete before build
+
 #install build deps
 npm_install = subprocess.Popen(shlex.split("npm install"), cwd=working_dir, shell=True)
 
-npm_install.wait() #wait to complete before build
+npm_install.wait()
+
+#add config options... Gotta decide how to update them to fit the new version
 
 #build scripts
 subprocess.Popen(shlex.split("grunt"), cwd=working_dir, shell=True)
