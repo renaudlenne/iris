19cd6ded9d79ef7dc68e7c46179460259b98fee5
megawac <megawac@gmail.com>
2014-03-19 02:05:20 -0400

    Compile static files via compile.py
    Pings https://github.com/megawac/qwebirc-enhancements/issues/17

---
+++ bin/merge.py
@@ -0,0 +1,17 @@
+def merge(a, b, path=None):
+    """
+    merges b into a
+    from http://stackoverflow.com/questions/7204805/python-dictionaries-of-dictionaries-merge
+    """
+    if path is None: path = []
+    for key in b:
+        if key in a:
+            if isinstance(a[key], dict) and isinstance(b[key], dict):
+                merge(a[key], b[key], path + [str(key)])
+            elif a[key] == b[key]:
+                pass # same leaf value
+            else:
+                raise Exception('Conflict at %s' % '.'.join(path + [str(key)]))
+        else:
+            a[key] = b[key]
+    return a
--- compile.py
+++ compile.py
@@ -1,4 +1,5 @@
-import subprocess, shlex
+import subprocess, shlex, yaml
+import bin.merge as merge
 
 working_dir = r"static"
 
@@ -12,6 +13,14 @@ npm_install = subprocess.Popen(shlex.split("npm install"), cwd=working_dir, shel
 npm_install.wait()
 
 #add config options... Gotta decide how to update them to fit the new version
+with open("frontend-config.yml") as cf:
+    pyconfig = yaml.load(cf)
+with open(working_dir + "/" + "app-config.yml") as working_config:
+    config = yaml.load(working_config)
+    merge(config, pyconfig) #prefer opts set in python instance
+with open(working_dir + "/" + "app-config.yml", "w") as working_config:
+    working_config.write( yaml.dump(config, default_flow_style=True) ) #write em out
+
 
 #build scripts
 subprocess.Popen(shlex.split("grunt"), cwd=working_dir, shell=True)
