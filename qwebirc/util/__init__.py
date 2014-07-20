4778dbcf06a552be6f8414855bc6f9a8332600d9
megawac <yeatesgraeme@gmail.com>
2014-07-19 20:07:05 -0400

    Fix gzip issue and update static

--- qwebirc/util/__init__.py
+++ qwebirc/util/__init__.py
@@ -1,1 +1,12 @@
 from hitcounter import HitCounter
+from gziprequest import GZipRequest
+
+def apply_gzip(request):
+  accept_encoding = request.getHeader('accept-encoding')
+  if accept_encoding:
+    encodings = accept_encoding.split(',')
+    for encoding in encodings:
+      name = encoding.split(';')[0].strip()
+      if name == 'gzip':
+        request = GZipRequest(request)
+  return request
